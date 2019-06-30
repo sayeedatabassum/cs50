05c02ba70ef6421e1853b90db48d3205dfca4068
sayeedatabassum <sayeedatabassum96@gmail.com>
2019-06-30 19:58:02 +0530

    added

--- lecture2/notes/application.py
+++ lecture2/notes/application.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, session
-from flask.ext.session import Session
+from flask_session import Session
 
 app = Flask(__name__)
 
@@ -7,13 +7,13 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-notes = []
-
 @app.route("/", methods=["GET", "POST"])
 def index():
+    if session.get("notes") is None:
+        session["notes"] = []
     if request.method == "POST":
         note = request.form.get("note")
-        notes.append(note)
+        session["notes"].append(note)
 
-    return render_template("index.html", notes=notes)
+    return render_template("index.html", notes=session["notes"])
 
